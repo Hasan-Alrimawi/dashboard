8bc7935ab062f19ea902871a3591f3f5f85c5dfa
Hasan Alrimawi <hasan.alrimawi@exalt.ps>
2022-10-23 16:51:03 +0300

    design & chart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,20 @@ import { AppComponent } from './app.component';
 import { RecordComponent } from './components/record/record.component';
 import { WorkingCapitalComponent } from './components/working-capital/working-capital.component';
 import { AnalyticsComponent } from './components/analytics/analytics.component';
-
+import {ChartModule} from 'primeng/chart';
+import { MainDashComponent } from './views/main-dash/main-dash.component';
 @NgModule({
   declarations: [
     AppComponent,
     RecordComponent,
     WorkingCapitalComponent,
-    AnalyticsComponent
+    AnalyticsComponent,
+    MainDashComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/analytics/analytics.component.ts
+++ src/app/components/analytics/analytics.component.ts
@@ -6,7 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./analytics.component.css']
 })
 export class AnalyticsComponent {
-  temp = { title: '# of N', statitics:[{ value: '97', tar: 'New Customers' }, 
-  {value:'110', tar:'Target'}, 
-  {value:'88%', tar:'Target Achievment'}]}
+  temp = {
+    title: '# of New Customers', statitics: [{ value: '97', tar: 'New Customers' },
+    { value: '110', tar: 'Target' },
+    { value: '88%', tar: 'Target Achievment' }]
+  }
+
+  basicData = {
+    labels: ['January', 'February', 'March'],
+    datasets: [
+      {
+        label: 'Last Period',
+        backgroundColor: '#42A5F5',
+        data: [35, 34, 30]
+      },
+      {
+        label: 'Current Period',
+        backgroundColor: '#FFA726',
+        data: [28, 48, 40]
+      }
+    ]
+  };
 }
